08f4b7c5c36ee22279a83b730aa2177bb7bbb1b9
rezkycx <rezky_cx@hotmail.com>
2024-10-04 18:55:13 +0700

    update

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,6 +1,6 @@
 // src/components/BaseLatout.jsx  
 import { Outlet } from "react-router-dom"
-import Navbar from "./Navbar.JSX"
+import Navbar from "./Navbar"
 export default function Template() {
     return (
         <div>
